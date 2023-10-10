7e16fd7b1021ef291b6dda463e14405e19369689
洛伦大叔 <95688743+luolundashu@users.noreply.github.com>
2023-10-10 10:22:02 +0800

    Add files via upload

--- ml_get_paper.py
+++ ml_get_paper.py
@@ -7,10 +7,6 @@ import os
 import requests
 import time
 from tqdm import tqdm
-# os.environ['HTTP_PROXY'] = "http://127.0.0.1:7890"
-# os.environ['HTTPS_PROXY'] = "http://127.0.0.1:7890"
-# 启动浏览器
-
 
 def contains_keyword(keyword, text):
     return keyword.lower() in text.lower()
@@ -19,17 +15,26 @@ def contains_keyword(keyword, text):
 def create_web(paper_list,conference_name,key_word,url_name,patience=20):
     if contains_keyword('iclr', conference_name):
         paper_dir_father = "./paper/{}/{}/{}/".format(conference_name,url_name,key_word)
-    else:
+    elif contains_keyword('NeurIPS', conference_name):
         paper_dir_father = "./paper/{}/{}/".format(conference_name, key_word)
+    elif contains_keyword('icml', conference_name):
+        paper_dir_father = "./paper/{}/{}/".format(conference_name, key_word)
+
     if os.path.exists(paper_dir_father) is not True:
         os.makedirs(paper_dir_father)
 
     for i in tqdm(range(len(paper_list))):
-        paper_name = paper_list[i].find('h4').text.strip()
+        if contains_keyword('icml', conference_name):
+            paper_name = paper_list[i].text.strip()
+        else:
+            paper_name = paper_list[i].find('h4').text.strip()
         paper_name = paper_name.replace('/', '')
         paper_name = paper_name.replace(':', '')
         if contains_keyword(key_word, paper_name):
-            paper_down_dir = paper_list[i].find('h4').find('a', class_='pdf-link')['href']
+            if contains_keyword('icml', conference_name):
+                paper_down_dir = paper_list[i].find('a', class_='pdf-link')['href']
+            else:
+                paper_down_dir = paper_list[i].find('h4').find('a', class_='pdf-link')['href']
             paper_down_dir = 'https://openreview.net{}'.format(paper_down_dir)
             for pat in range(patience):
                 response = requests.get(paper_down_dir)
@@ -62,7 +67,7 @@ def crete_url(conference_name):
 
 if __name__ =='__main__':
     keyword_list = ['Diffusion','Anomaly Detection','Time Series','Bayesian','Out of Distribution']
-    conference_name_list =['NeurIPS2022','ICML2023','ICLR2023']
+    conference_name_list =['ICML2023']
     options = Options()
     driver = webdriver.Chrome(service=Service('./chromedriver-win64/chromedriver.exe'), options=options)
     os.environ['HTTP_PROXY'] = "http://127.0.0.1:7890"
@@ -80,20 +85,30 @@ if __name__ =='__main__':
                 for i in range(1000):
                     # 获取当前页面的 HTML
                     i = i +1
-                    try :
-                        next_page_button = driver.find_element(By.CSS_SELECTOR, 'li[data-page-number="{}"] a'.format(i))
-                        next_page_button.click()
-                        print('会议：{}__ 关键词{}加载第{}页'.format(conference_name,keyword,i))
-                        time.sleep(3)
-                    except Exception as e:
-                        print('*'*20)
-                        print('会议：{}__ 关键词{}已经全部爬取完成'.format(conference_name,keyword))
-                        print('*'*20)
-                        break
+                    if i>1:
+                        try :
+                            if contains_keyword('icml',conference_name):
+                                next_page_button = driver.find_element(By.XPATH,
+                                                                       '//li[not(@class="active")]/a[@role="button" and text()="{}"]'.format(
+                                                                           i))
+                            else:
+                                next_page_button = driver.find_element(By.CSS_SELECTOR, 'li[data-page-number="{}"] a'.format(i))
+                            next_page_button.click()
+                            print('会议：{}__ 关键词{}加载第{}页'.format(conference_name,keyword,i))
+                            time.sleep(3)
+                        except Exception as e:
+                            print('*'*20)
+                            print('会议：{}__ 关键词{}已经全部爬取完成'.format(conference_name,keyword))
+                            print('*'*20)
+                            break
 
                     page_content = driver.page_source
                     soup = BeautifulSoup(page_content, 'html.parser')
-                    paper_list = soup.find_all('li',class_='note')
-                    create_web(paper_list,conference_name,keyword,url_name)
+                    if contains_keyword('icml',conference_name):
+                        paper_list = soup.find_all('ul', class_='list-unstyled list-paginated')[0].find_all('h4')
+                        create_web(paper_list, conference_name, keyword, url_name)
+                    else:
+                        paper_list = soup.find_all('li',class_='note')
+                        create_web(paper_list,conference_name,keyword,url_name)
 
 
